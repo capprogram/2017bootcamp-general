c90583c75ee88e364c3b1b6498f4cc750e0eef8c
Sheila Kannappan <sheila@physics.unc.edu>
2017-06-08 14:46:05 -0400

    python best practices and debugging

---
+++ bestpractices.md
@@ -0,0 +1,46 @@
+# Programming Best Practices
+
+## Goals
+
+* Easy debugging
+* Easy modification
+* Understandability (now, after passage of time, and to another person)
+* Speed
+
+## Strategies
+* Plan – consider likely rate-limiting steps and best methodology before starting
+* Modularize – test subcomponents and use branches in Git
+* Check variable values, types, array sizes by hand (print statements or interrupted run)
+* Don’t assume “running” = “working”; brainstorm sanity checks
+* Use meaningful variable names (more than one letter!) that are not too similar
+* Keep standard defaults: e.g. i, j reserved for integer loop counters
+* Replace hardwired numbers with constant names at top of code or even in separate file
+* Write comments (including to bookend command sets, e.g. in if-then)
+* Take advantage of helpful visual appearance: white space, syntax highlighting
+* Avoid loops when unnecessary (possible tradeoff with understandability)
+* Manage I/O and memory; eliminate large arrays when no longer needed
+* Use print and system time statements to find out where code fails/slows
+
+## Python-specific tips ##
+* avoid "import" for scripts -- they will run at the time of import! use the "def main" protocol if you want your code to be both callable like a script and importable like a package -- see [this link](https://en.wikibooks.org/wiki/Python_Programming/Modules)
+* don't define a function in the middle of a program, even though python lets you -- doing this muddles whether variables are defined inside or outside the function
+* you can't use the variable name "lambda", it's a reserved word
+* do not use tabs as white space; standard indentation of code levels is 4 spaces (this is a matter of preference, but standardizing is essential when collaborating)
+* when speed is critical use libraries written in C and/or packages and functions set up to multi-thread
+* debug using the python debugger module "pdb" ([quick tutorial here](https://pythonconquerstheuniverse.wordpress.com/category/python-debugger/))
+* time code using the system clock, for example: 
+
+```python
+import numpy as np
+import time
+
+init_time = time.clock()  # start clock
+x = np.linspace(0,100,1000000)
+y = np.sqrt(x)
+
+elap_time = time.clock() - init_time  # finds difference
+
+print "Time elapsed is %0.3f ms" % (elap_time*1000)  # converts to ms
+```
+
+or in ipython try using the [`%time`](https://ipython.org/ipython-doc/3/interactive/magics.html#magic-time) and [`%timeit`](https://ipython.org/ipython-doc/3/interactive/magics.html#magic-timeit) magics
---
+++ templatecode.py
@@ -0,0 +1,129 @@
+"""
+This is a template code for ASTR 503/703. It is intended to illustrate
+standard imports and header information, while providing practice in
+debugging, speed optimization, and spotting bad habits in programming.
+
+This code runs but is deeply flawed. Perform the four tasks below to
+learn something about both programming and the Central Limit Theorem.
+
+Author: Sheila Kannappan
+Created: August 2016
+"""
+
+# standard imports and naming conventions; uncomment as needed
+import numpy as np              # basic numerical analysis
+import matplotlib.pyplot as plt # plotting
+#import scipy as sp              # extended scientific function
+import scipy.stats as stats     # statistical functions
+#import numpy.random as npr      # random number generation
+#import astropy as ap            # core astronomy library
+#import astroML as ml            # machine learning for astronomy
+#import astroML.datasets as mld  # datasets
+#import pymc                     # bayesian package with MCMC
+#import pdb                      # python debugger
+#import time                     # python timekeeper
+#plt.ion()                       # use if working in ipython under linux
+
+# if any package above does not import properly, then you need to
+# revisit your anaconda installation
+
+"""
+Just for fun, this code will explore the Central Limit Theorem by comparing 
+Poisson distributions with Gaussian distributions.
+
+An example of a Poisson process is counting the # of people who use the gym per
+hour where the count is run for nhr = different times. We assume the underlying
+average users per hour U is fixed but the counts have "Poisson fluctuations"
+so N = nhr x U specifies the *expected* count (the mean of the theoretical
+Poisson distribution) not the observed count Nobs, whose possible values have 
+different probabilities following a Poisson distribution with mean N. (In fact
+nhr may need to be very large for N to exactly equal Nobs, because Nobs is by
+definition an integer whereas U is by definition a real number.)
+
+Statistical theory tells us that for a Poisson process, the observed count Nobs 
+fluctuates around the true theoretical mean N with a 68% confidence interval of 
++-sqrt(N) for "large N". The sleight of hand of statistics is to use the
+observed data to estimate N as Nobs and likewise estimate the 68% confidence 
+interval as +-sqrt(Nobs). Thus the estimated fractional error in the count is 
+fracerr = sqrt(Nobs)/Nobs = 1/sqrt(Nobs). The 1/sqrt explains why we get a 
+better estimate of U by running the count for 10 hours rather than 1 hour. 
+However in the exercise below we will not use data but simply compare the 
+theoretical distributions while increasing N (or equivalently, increasing nhr).
+
+According to the Central Limit Theorem, as we increase N, the Poisson 
+distribution should start to look like a Gaussian. Therefore we will plot the
+Poisson distribution for increasing N and overplot Gaussians with the same mean
+N and 68% confidence interval +-sqrt(N), to see how quickly the Poisson shape
+approaches a Gaussian shape (i.e., when are we in the "large N" limit).
+"""
+    
+def gaussfunc(xvals, mean, sigma):
+    y = np.exp(-1.*(((xvals-mean)**2) / (2.* sigma**2)))
+    norm = 1./np.sqrt(2. * sigma**2 * np.pi)
+    y = norm * y
+    return y
+
+U = 8. # underlying rate of gym users per hour
+N = np.array([6, 36, 216, 1296]) # total number of people counted (powers of 6)
+nhr = N/U # time to count this many people
+#labelarr = ["count for %s hr" % ihr for ihr in nhr]
+
+for i in xrange(0, len(N)):
+    
+    # plot probabilities of count values for range around mean
+    mean = N[i]
+    maxval = 2*mean
+    xvals=np.arange(0, maxval)
+    def poissonfunc(xvals, mean):
+        prob=np.zeros(len(xvals))
+        for j in range(0, len(xvals)):
+            prob[j] = stats.poisson.pmf(xvals[j], mean)
+        return prob
+    prob = poissonfunc(xvals, mean)
+    plt.plot(xvals, prob, 'r', lw=3)
+    plt.xlabel("count value")
+    plt.ylabel("probability")
+    plt.xscale("log")
+    sel = np.where(prob == max(prob))
+    n = xvals[sel]
+    probval = prob[sel]
+    label = "count for %s hr" % (nhr[i])
+    plt.text(n, probval, label)
+    
+# plot Gaussian distribution with matching mean and sigma
+sigma=np.sqrt(mean)
+y = gaussfunc(xvals, mean, sigma)
+plt.plot(xvals, y, 'b')
+
+
+"""
+Task 1: Many times a code runs fine, but the output may be wrong; you 
+step through it line by line to make sure it's doing what you think it 
+should be doing. There are several errors in the program above. Try 
+to find them using pdb.set_trace() as described in the tutorial here:
+https://pythonconquerstheuniverse.wordpress.com/category/python-debugger/
+Check the size and contents of the variables at each step to determine 
+whether they make sense. Useful commands include print, len(), and 
+np.size(). WATCH OUT: the very first bug you need to find is one that
+makes pdb not even work properly -- why does this code mess up how
+the next line ("n") command works in pdb?
+
+Task 2: We don't always want to optimize code speed -- sometimes it's
+just not important -- but you should be in the habit of avoiding 
+silly things that slow your code down, like unnecessary loops or math
+operations. Use time.clock() to measure the time taken by the whole code,
+and each part of the code, above and try to find inefficiencies. When 
+you find a slow step, ask yourself whether it could be faster, and 
+whether it matters (is it the rate-limiting step?). For now, fix it 
+even if it's not the rate-limiting step, just for practice. Overall,
+you should be able to speed up this code by about a factor of 10.
+
+Task 3: Some things in the code above represent poor programming practice,
+even though they do not affect speed and are not bugs. Note examples and
+correct them.
+
+Task 4: Once you've got the code fixed up, you can play with the zoom in the
+plot window to see how closely the Poisson and Gaussian distributions match 
+each other for each value of N. If they match well, does that mean the 
+fractional error in the observed count must be small? Explain.
+"""
