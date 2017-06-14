cca5600116814675cc070b22571687e54d8b04ae
Sheila Kannappan <sheila@physics.unc.edu>
2017-06-13 20:15:18 -0400

    more bootstrapping tutorial code

--- paramfit1_boot.py
+++ paramfit1_boot.py
@@ -1,11 +1,13 @@
 """
 Code for Tutorial on Parameter Estimation by Maximum Likelihood Fitting
 Modified by Kathleen Eckert from an activity written by Sheila Kannappan
+++version with BOOTSTRAPPING added by Sheila Kannappan June 2017
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.random as npr
+from astroML.utils import check_random_state
 
 # Generating fake data set to start with:
 alphatrue=2. # slope
@@ -15,13 +17,6 @@ errs=2.5 # sigma (amplitude of errors)
 narr=50 # number of data points
 xvals = np.arange(narr) + 1. # xvals range from 1-51
 yvals = alphatrue*xvals + betatrue + npr.normal(0,errs,narr) # yvals 
-# What aspect of a real data set does npr.normal emulate here?
-# What assumption is made here that is key to the least squares approach?
-'''
-It's emulating random measurement errors with amplitude sigma=errs.
-The code is assuming that these errors have the same amplitude for all 
-data points, which is key to the unweighted least squares approach.
-'''
 
 # Plot fake data
 plt.figure(1) 
@@ -35,12 +30,6 @@ plt.ylabel("y-values")
 alphaest=(np.mean(xvals)*np.mean(yvals)-np.mean(xvals*yvals)) / \
    (np.mean(xvals)**2 -np.mean(xvals**2)) #  from derivation
 betaest= np.mean(yvals) - alphaest * np.mean(xvals) # calculate estimate of y-intercept from derivation
-# Why must we use alphaest rather than alphatrue in the above formula?
-'''
-These formulae represent our best *estimates* of the slope and intercept from 
-the *data*. We do not a priori know the true slope when working with real 
-data, so we cannot use our knowledge of it in this analysis.
-'''
 
 # The MLE values of the slope and y-intercept are equivalent to the "least
 # squares" fit results.
@@ -61,10 +50,6 @@ print("analytical MLE uncertainty on beta is %0.7f" % (betaunc))
 
 print("fractional uncertainty on alpha is %0.7f" % (alphaunc/alphaest))
 print("fractional uncertainty on beta is %0.7f" % (betaunc/betaest))
-# Which parameter has larger fractional uncertainty?
-'''
-The intercept beta has much larger fractional uncertainty.
-'''
 
 # Solution using python solver np.polyfit
 # third parameter is order of fit, 1 for linear
@@ -74,35 +59,35 @@ print("               ") # put in some whitespace to make easier to read
 print("np.polyfit MLE slope = %0.7f" %pfit[0])
 print("np.polyfit MLE y-intercept = %0.7f" %pfit[1])
 
-# Do you get the same result as in analytical case?
-'''
-Yes, the result is the same.
-'''
-
-# Note that most problems do not have analytical solutions
-
 # Can also obtain parameter uncertainties from the diagonal terms of the covariance
 # matrix, which is the inverse of the Hessian matrix and
 # can be computed in np.polyfit by setting cov='True'
 
 pfit,covp = np.polyfit(xvals, yvals, 1, cov='True') # returns coeff. of highest power first
 # setting cov='True' returns the covariance matrix
-# how do we get the errors from it?
 print("slope is %0.7f +- %0.7f" % (pfit[0], np.sqrt(covp[0,0])))
 print("intercept is %0.7f +- %0.7f" % (pfit[1], np.sqrt(covp[1,1])))
-'''
-The error estimate comes from the square root of the corresponding diagonal term.
-'''
 
-# Are the uncertainties the same as in the analytical solution?
-'''
-They agree closely but are not identical.
-'''
-# What happens to the uncertainties if you increase/decrease the number of points used in the fit (try N=100, N=10) ?
-'''
-The uncertainties get larger for N=10, smaller for N=100.
-'''
-# What happens to the percentage difference between the analytical and numerical methods for computing the uncertanties if you increase/decrease the number of points (try N=100, N=10)?
-'''
-The percentage difference between methods gets larger for N=10, smaller for N=100.
-'''
+# BOOTSTRAP!
+
+npars = 2 # slope and intercept
+nboot = 10000 # usually want at least 1000
+rng = check_random_state(None)
+ind = rng.randint(narr, size=(narr,nboot))
+bootresults = np.zeros((npars,nboot))
+for iboot in xrange(nboot):
+    bootresults[:,iboot] = np.polyfit(xvals[ind[:,iboot]], yvals[ind[:,iboot]], 1)
+sloperesults = bootresults[0,:]
+intresults = bootresults[1,:]
+slopesort = np.argsort(sloperesults)
+slopemed = np.median(sloperesults)
+pct16 = int(round(0.16*nboot))
+pct84 = int(round(0.84*nboot))
+slope68percentconf = [slopemed-sloperesults[slopesort[pct16]],sloperesults[slopesort[pct84]]-slopemed]
+intsort = np.argsort(intresults)
+intmed = np.median(intresults)
+int68percentconf = [intmed-intresults[intsort[pct16]],intresults[intsort[pct84]]-intmed]
+
+print("               ") # put in some whitespace to make easier to read
+print("bootstrap slope is %0.7f + %0.7f - %0.7f" % (slopemed,slope68percentconf[0],slope68percentconf[1]))
+print("bootstrap intercept is %0.7f + %0.7f - %0.7f" % (intmed,int68percentconf[0],int68percentconf[1]))
