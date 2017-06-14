b70ce6e5a76aa1f3d8295b229929ef87c16f4815
Sheila Kannappan <sheila@physics.unc.edu>
2017-06-14 11:16:45 -0400

    add multiprocessing version of bootstrapped line fits

---
+++ paramfit1_boot_mp.py
@@ -0,0 +1,92 @@
+"""
+Code for Tutorial on Parameter Estimation by Maximum Likelihood Fitting
+Modified by Kathleen Eckert from an activity written by Sheila Kannappan
+++version with BOOTSTRAPPING added by Sheila Kannappan June 2017
+++version with MULTIPROCESSING added by Sheila Kannappan June 2017
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.random as npr
+from astroML.utils import check_random_state
+import multiprocessing as mp
+import time
+
+def newdatafullbootstrap(runindex):
+    # NOTE: all print and plot outputs removed, apparently blocked by
+    # multiprocessing anyway
+    
+    # Generating fake data set to start with:
+    alphatrue=2. # slope
+    betatrue=5.  # intercept
+    errs=2.5 # sigma (amplitude of errors)
+    
+    narr=50 # number of data points
+    xvals = np.arange(narr) + 1. # xvals range from 1-51
+    yvals = alphatrue*xvals + betatrue + npr.normal(0,errs,narr) # yvals 
+    
+    # Determine slope & y-intercept using least squares analytic solution 
+    alphaest=(np.mean(xvals)*np.mean(yvals)-np.mean(xvals*yvals)) / \
+       (np.mean(xvals)**2 -np.mean(xvals**2)) #  from derivation
+    betaest= np.mean(yvals) - alphaest * np.mean(xvals) # calculate estimate of y-intercept from derivation
+    # The MLE values of the slope and y-intercept are equivalent to the "least
+    # squares" fit results.
+    
+    # Compute analytic uncertainties on slope and y-intercept 
+    alphaunc = np.sqrt(np.sum((yvals - (alphaest*xvals+betaest))**2) / ((narr-2.)*(np.sum((xvals-np.mean(xvals))**2))))
+    betaunc = np.sqrt((np.sum((yvals - (alphaest*xvals+betaest))**2) / (narr-2.)) * ((1./narr) + (np.mean(xvals)**2)/np.sum((xvals-np.mean(xvals))**2)) )
+    
+    # Solution using python solver np.polyfit
+    # third parameter is order of fit, 1 for linear
+    pfit = np.polyfit(xvals, yvals, 1) # returns coeff. of highest order term first
+    
+    # Can also obtain parameter uncertainties from the diagonal terms of the covariance
+    # matrix, which is the inverse of the Hessian matrix and
+    # can be computed in np.polyfit by setting cov='True'
+    pfit,covp = np.polyfit(xvals, yvals, 1, cov='True') # returns coeff. of highest power first
+    # setting cov='True' returns the covariance matrix
+    
+    # BOOTSTRAP!
+    
+    npars = 2 # slope and intercept
+    nboot = 10000 # usually want at least 1000
+    rng = check_random_state(None)
+    ind = rng.randint(narr, size=(narr,nboot))
+    bootresults = np.zeros((npars,nboot))
+    for iboot in xrange(nboot):
+        bootresults[:,iboot] = np.polyfit(xvals[ind[:,iboot]], yvals[ind[:,iboot]], 1)
+    sloperesults = bootresults[0,:]
+    intresults = bootresults[1,:]
+    slopesort = np.argsort(sloperesults)
+    slopemed = np.median(sloperesults)
+    pct16 = int(round(0.16*nboot))
+    pct84 = int(round(0.84*nboot))
+    slope68pcterrs = [slopemed-sloperesults[slopesort[pct16]],sloperesults[slopesort[pct84]]-slopemed]
+    intsort = np.argsort(intresults)
+    intmed = np.median(intresults)
+    int68pcterrs = [intmed-intresults[intsort[pct16]],intresults[intsort[pct84]]-intmed]
+    
+    slope_err_ratio = 0.5*(np.sum(slope68pcterrs))/np.sqrt(covp[0,0])
+    int_err_ratio = 0.5*(np.sum(int68pcterrs))/np.sqrt(covp[1,1])
+    slope_err_ratio2 = 0.5*(np.sum(slope68pcterrs))/alphaunc
+    int_err_ratio2 = 0.5*(np.sum(int68pcterrs))/betaunc
+    
+    return runindex, slope_err_ratio, int_err_ratio
+
+if __name__ == '__main__':
+    init_time = time.clock()  # start clock
+    pool = mp.Pool(4)
+    results = pool.map(newdatafullbootstrap, range(50))
+    tupletypes = np.dtype('int, float, float')
+    mixedarray = np.array(results, dtype=tupletypes)
+    runindex = mixedarray['f0']
+    slope_err_ratio = mixedarray['f1']
+    int_err_ratio = mixedarray['f2']
+    # Plot ratios
+    plt.figure(2) 
+    plt.clf()
+    plt.plot(slope_err_ratio,int_err_ratio,'b*',markersize=10)
+    plt.xlabel("slope error ratio")
+    plt.ylabel("intercept error ratio")
+    elapsed_time = time.clock() - init_time
+    print "elapsed time (ms) %0.3f" % (1000.*elapsed_time)
