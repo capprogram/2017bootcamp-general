ddbad47fa823e371cd3e86581abb7259c39d35ee
Sheila Kannappan <sheila@physics.unc.edu>
2017-06-14 20:19:16 -0400

    update mptest.py

--- mptest.py
+++ mptest.py
@@ -13,9 +13,11 @@ def funSquare(num):
     return num ** 2
 
 if __name__ == '__main__':
-    nums=range(30000)     
+    nums=range(30000)
+    tic0 = time.clock()
     poola = multiprocessing.Pool(1) #initializes N workers, where N = # of CPU cores
     poolb = multiprocessing.Pool(3) #initializes N workers, where N = # of CPU cores
+    toc0 = time.clock()
     tic1 = time.clock()
     results1 = poola.map(funSquare, nums)
     toc1 = time.clock()
@@ -28,5 +30,6 @@ if __name__ == '__main__':
     tic4 = time.clock()
     resultsman = funSquare(np.array(nums))
     toc4 = time.clock()
+    print('Pool setup time: %r' % (toc0 - tic0))
     print('Parallel processing time (1, 3 proc): %r %r\nSerial processing time (map, manual): %r %r'
           % (toc1 - tic1, toc2 - tic2, toc3 - tic3, toc4 - tic4))
